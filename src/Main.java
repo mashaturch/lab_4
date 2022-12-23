cdd7fc0dd3ad30ee8420afd652571e52201f227f
mashaturch <s1yguvzg>
2022-12-24 02:24:40 +0700

    create Main

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        FileAccountManager file = new FileAccountManager();
+
+        //Регистрация нового аккаунта.
+        Account account1 = new Account("Мария Александровна Турчинович", "05.06.2002", "mashaturch@gmail.com", "1234");
+        try {
+            file.register(account1);
+        } catch (AccountAlreadyExistsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(FileService.getInstance().readFile());
+
+        //Проверка метода FailedLoginCounter
+        for (int i = 0; i < 5; i++) {
+            try {
+                System.out.println(file.login("mashat@gmail.com", "12345"));
+                System.out.println(account1.getCount());
+
+            } catch (AccountBlockedException | WrongCredentialsException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        //Верная попытка входа в аккаунт
+        try {
+            System.out.println(file.login("mashaturch@gmail.com", "1234"));
+            System.out.println(FailedLoginCounter.getInstance("mashaturch@gmail.com"));
+        } catch (AccountBlockedException | WrongCredentialsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Проверка метода FailedLoginCounter
+        for (int i = 0; i < 5; i++) {
+            try {
+                System.out.println(file.login("mashaturch@gmail.com", "123"));
+
+            } catch (AccountBlockedException | WrongCredentialsException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // Попытка входа в уже заблокиированный аккаунт
+        try {
+            System.out.println(file.login("mashaturch@gmail.com", "12345"));
+        } catch (AccountBlockedException | WrongCredentialsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
